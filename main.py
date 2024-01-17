806fe2d0a42de91d4f5aa0451180b6b2f5306243
EduardoRivas98 <joserivs1998@gmail.com>
2024-01-16 19:53:15 -0600

    se lee el archivo .csv

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+import pandas as pd
+from io import BytesIO
 
 app = FastAPI()
 
+# HTML que se muestra
+html_form = """
+<form method="post" enctype="multipart/form-data" action="/uploadfile/">
+    <label for="file">Subir CSV: </label>
+    <input type="file" name="file" accept=".csv">
+    <input type="submit">
+</form>
+"""
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    df = pd.read_csv(BytesIO(contents))
+    return {"filename": file.filename, "rows": len(df)}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/form/")
+async def form_post():
+    return HTMLResponse(content=html_form, status_code=200)
